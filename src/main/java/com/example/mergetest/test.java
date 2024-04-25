6ec16b2b7990a16963911299238969b299142e89
doy7m <d.m1999y@gmail.com>
2024-04-25 22:27:57 +0300

    4 commit

--- src/main/java/com/example/mergetest/test.java
+++ src/main/java/com/example/mergetest/test.java
@@ -2,12 +2,7 @@ package com.example.mergetest;
 
 public class test {
 
-
-    public void fullName(){
-        System.out.println("NaWAF");
-
-    }
-    public void age(){
-        System.out.println(1);
+    public String namename(){
+        return "meow";
     }
 }
