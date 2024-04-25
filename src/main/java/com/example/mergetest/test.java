0792a12d55b5c7df17eb7eeea9d8ae2774ece858
doy7m <d.m1999y@gmail.com>
2024-04-25 22:13:02 +0300

    first commit

---
+++ src/main/java/com/example/mergetest/test.java
@@ -0,0 +1,9 @@
+package com.example.mergetest;
+
+public class test {
+
+
+    public void name(){
+        System.out.println("nawaf");
+    }
+}
---
+++ src/test/java/com/example/mergetest/MergeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mergetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
